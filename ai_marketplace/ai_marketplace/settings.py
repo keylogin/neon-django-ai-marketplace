6c5caa7c8296ce46a03029e4f45c286b859482e9
keylogin <seriousam_007@yahoo.com>
2025-02-20 19:14:48 +0200

    .

--- ai_marketplace/ai_marketplace/settings.py
+++ ai_marketplace/ai_marketplace/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dl^s$r2w_#ttxljw)+in%anjy$y6ul^5m-bm*7qrfwrq+@&jbr'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
